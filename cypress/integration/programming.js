b3d6f68657ea5a5f84c149f1e20d98febef61ae8
Daniel Kiss <littlesnorrboy@gmail.com>
2020-04-12 19:31:06 +0200

    Tweak tests

--- cypress/integration/programming.js
+++ cypress/integration/programming.js
@@ -1,15 +1,25 @@
 describe("Can compile a simple program", () => {
   it("loads", () => {
+    cy.server();
+    cy.route("GET", "/myself", "fixture:myself.json").as("myself");
+
     cy.visit("/");
-  });
-  it("navigates to programming", () => {
-    cy.get("a[id=programming-link]").click();
+
+    cy.wait("@myself").then((xhr) => {
+      assert.equal(xhr.status, 200);
+    });
   });
   it("can compile a 'pass-instruction'", () => {
+    cy.server();
+    cy.route("GET", "/myself", "fixture:myself.json");
+    cy.route("GET", "/script/my_scripts", []);
+    cy.route("POST", "/script/compile").as("compile");
+
+    cy.visit("/#/programming");
     cy.get("[id=schema-node-Pass]").click();
-  });
-  it("compiles successfully", () => {
-    let result = cy.get("[id=compilation-result]");
-    result.should('contain', "Compiled successfully")
+    cy.wait("@compile").then((xhr) => {
+      let result = cy.get("[id=compilation-result]");
+      result.should("contain", "Compiled successfully");
+    });
   });
 });
