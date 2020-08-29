7bd13f8e74f06389c3987d7af57b1377456193fc
Kiss Balint <bk@creygames.com>
2020-08-29 05:47:22 +0200

    adding world objects

---
+++ src/Game/Bot.js
@@ -0,0 +1,20 @@
+import { Graphics } from "pixi.js";
+import { PixiComponent } from "@inlet/react-pixi";
+
+const Bot = PixiComponent("Bot", {
+    create: (props) => new Graphics(),
+    applyProps: (instance, _, props) => {
+        const { x, y, mouseOver } = props;
+
+        instance.clear();
+        instance.beginFill(0xff3300);
+        instance.drawCircle(0, 0, 3);
+        instance.endFill();
+        instance.x = x;
+        instance.y = y;
+        instance.interactive = true;
+        instance.on("mouseover", mouseOver);
+    },
+});
+
+export default Bot;
