7eb6e95930d690f43664bbc751a06e582b4b465c
Kiss Balint <bk@creygames.com>
2020-09-08 19:42:54 +0200

    basic bot selection

--- src/Game/Bots.js
+++ src/Game/Bots.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { Graphics } from "pixi.js";
-import { PixiComponent } from "@inlet/react-pixi";
+import { PixiComponent, useApp } from "@inlet/react-pixi";
 import { caoMath } from "../CaoWasm";
 import { apiBaseUrl } from "../Config";
 import axios from "axios";
@@ -8,20 +8,35 @@ import axios from "axios";
 const Bot = PixiComponent("Bot", {
     create: (props) => new Graphics(),
     applyProps: (instance, _, props) => {
-        const { x, y, size, mouseOver } = props;
+        const { x, y, size, selected, mouseDown, mouseEnter, mouseLeave } = props;
 
         const hexagonRadius = Math.sqrt(3) * size + 0.55;
         instance.clear();
+        if (selected) {
+            instance.beginFill(0x00ff33);
+            instance.drawCircle(x + hexagonRadius - 2, y + hexagonRadius, hexagonRadius / 1.5 + 5);
+            instance.endFill();
+        }
         instance.beginFill(0xff3300);
         instance.drawCircle(x + hexagonRadius - 2, y + hexagonRadius, hexagonRadius / 1.5);
         instance.endFill();
         instance.interactive = true;
-        instance.on("mouseover", mouseOver);
+        instance.on("mousedown", mouseDown);
+        instance.on("mouseover", mouseEnter);
+        instance.on("mouseout", mouseLeave);
     },
 });
 
-function Bots({ room }) {
+function Bots({ room, setSelectedBot }) {
     const [bots, setBotData] = useState([]);
+    const app = useApp();
+    const setCursor = useCallback(
+        (cursorName) => {
+            app.renderer.plugins.interaction.cursorStyles.default = cursorName;
+            app.renderer.plugins.interaction.setCursorMode(cursorName);
+        },
+        [app]
+    );
 
     const fetchBotData = useCallback(async () => {
         const response = await axios.get(apiBaseUrl + "/bots", { params: room });
@@ -37,6 +52,8 @@ function Bots({ room }) {
         return () => clearInterval(interval);
     }, [fetchBotData]);
 
+    const [selectedId, setSelectedId] = useState();
+
     return (
         <>
             {bots.map((bot) => {
@@ -45,7 +62,22 @@ function Bots({ room }) {
                 const { x, y } = caoMath.axialToPixelMatrixPointy().rightProd(v);
                 const scaledX = x * Math.sqrt(3) * scale;
                 const scaledY = y * Math.sqrt(3) * scale;
-                return <Bot key={bot.id} size={10} x={scaledX} y={scaledY} color={0x000000} mouseOver={() => console.log(bot)} />;
+                return (
+                    <Bot
+                        key={bot.id}
+                        selected={bot.id === selectedId}
+                        size={10}
+                        x={scaledX}
+                        y={scaledY}
+                        color={0x000000}
+                        mouseDown={() => {
+                            setSelectedId(bot.id);
+                            setSelectedBot(bot);
+                        }}
+                        mouseEnter={() => setCursor("pointer")}
+                        mouseLeave={() => setCursor("auto")}
+                    />
+                );
             })}
         </>
     );
--- src/Game/RoomView.jsx
+++ src/Game/RoomView.jsx
@@ -1,54 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Stage, Container } from "@inlet/react-pixi";
 import Bots from "./Bots";
-import { apiBaseUrl } from "../Config";
-import axios from "axios";
 import styled from "styled-components";
 import FPSMeter from "./FPSMeter";
 import Terrain from "./Terrain";
 import Buttons2 from "./Buttons";
-
-const StyledRoomChooser = styled.div`
-    display: flex;
-    flex-direction: column;
-    height: 100px;
-    width: 200px;
-    overflow: auto;
-`;
-
-const StyledRoom = styled.div`
-    &:hover {
-        background: blue;
-    }
-`;
-
-function RoomChooser({ setSelected }) {
-    const [selectedRoom, setSelectedRoom] = useState();
-    useEffect(() => {
-        setSelected(selectedRoom);
-    }, [selectedRoom, setSelected]);
-
-    const [rooms, setRooms] = useState([]);
-    useEffect(() => {
-        const fetchRooms = async () => {
-            const response = await axios.get(apiBaseUrl + "/terrain/rooms");
-            setRooms(response.data);
-            setSelectedRoom(response.data[0]);
-        };
-        fetchRooms();
-    }, []);
-
-    return (
-        <div>
-            <div>{JSON.stringify(selectedRoom)}</div>
-            <StyledRoomChooser>
-                {rooms.map((room) => (
-                    <StyledRoom onClick={(e) => setSelectedRoom(JSON.parse(e.target.innerHTML))}>{JSON.stringify(room)}</StyledRoom>
-                ))}
-            </StyledRoomChooser>
-        </div>
-    );
-}
 
 const StyledRoomView = styled.div`
     position: relative;
@@ -60,19 +16,28 @@ const CanvasContainer = styled.div`
 
 function RoomView({ terrain, world }) {
     const [selectedRoom, setSelectedRoom] = useState();
+    const [selectedBot, setSelectedBot] = useState();
 
     return (
         <>
-            <RoomChooser setSelected={setSelectedRoom}></RoomChooser>
             <div>{JSON.stringify(selectedRoom)}</div>
+            <div>{JSON.stringify(selectedBot)}</div>
 
             <StyledRoomView>
                 <Buttons2 selectedRoom={selectedRoom} setSelected={setSelectedRoom}></Buttons2>
                 <CanvasContainer>
-                    <Stage width={500} height={450} options={{ backgroundColor: 0x486988, sharedTicker: true, antialias: true }}>
+                    <Stage
+                        width={500}
+                        height={450}
+                        options={{
+                            backgroundColor: 0x486988,
+                            sharedTicker: true,
+                            antialias: true,
+                        }}
+                    >
                         <Container scale={0.5} position={[-130, 0]}>
                             <Terrain room={selectedRoom}></Terrain>
-                            <Bots room={selectedRoom}></Bots>
+                            <Bots room={selectedRoom} setSelectedBot={setSelectedBot}></Bots>
                         </Container>
                         <FPSMeter></FPSMeter>
                     </Stage>
