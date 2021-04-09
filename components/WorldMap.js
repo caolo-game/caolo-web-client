85efa93e463b1f93043b6b60b104bb77a75b82b3
Daniel Kiss <littlesnorrboy@gmail.com>
2021-04-09 10:46:37 +0200

    Fix world map hexes

--- components/WorldMap.js
+++ components/WorldMap.js
@@ -1,59 +1,56 @@
-import React, { useMemo } from "react";
-import HexTile from "./HexTile";
 import ForEachHex from "./ForEachHex";
 
 const SQRT_3 = Math.sqrt(3);
 
 export default function WorldMap({ rooms }) {
-    const scale = 7;
-    return (
-        <>
-            <div
-                style={{
-                    width: "80vw",
-                    height: "80vh",
-                }}
-            >
-                <svg viewBox={`0 0 1000 600`}>
-                    <ForEachHex
-                        orientation="flat"
-                        pos={rooms.map(({ q, r }) => [q, r])}
-                        data={rooms}
-                        scale={scale}
-                    >
-                        <RoomTile />
-                    </ForEachHex>
-                </svg>
-            </div>
-        </>
-    );
+  const scale = 7;
+  return (
+    <>
+      <div
+        style={{
+          width: "80vw",
+          height: "80vh",
+        }}
+      >
+        <svg viewBox={`0 0 1000 600`}>
+          <ForEachHex
+            orientation="flat"
+            pos={rooms.map(({ q, r }) => [q, r])}
+            data={rooms}
+            scale={scale}
+          >
+            <RoomTile scaleOverride={6} />
+          </ForEachHex>
+        </svg>
+      </div>
+    </>
+  );
 }
 
-function RoomTile({ pos, pixelPos, data: _data, scale }) {
-    const width = scale * 2;
-    const height = scale * SQRT_3;
+function RoomTile({ pos, pixelPos, data: _data, scaleOverride: scale }) {
+  const width = scale * 2;
+  const height = scale * SQRT_3;
 
-    // TODO: flat top hexes pls
-    const points = [
-        [0, 0],
-        [width / 2, height / 4],
-        [width, 0],
-        [width, -height / 2],
-        [width / 2, (-height * 3) / 4],
-        [0, -height / 2],
-    ].map(([x, y]) => [x + pixelPos.x, y + pixelPos.y]);
+  const points = [
+    [0, 0],
+    [width / 4, height / 2],
+    [(width * 3) / 4, height / 2],
+    [width, 0],
+    [(width * 3) / 4, -height / 2],
+    [width / 4, -height / 2],
+  ].map(([x, y]) => [x + pixelPos.x, y + pixelPos.y]);
 
-    let path = `M ${points[0][0]} ${points[0][1]}`;
-    for (let pos of points.slice(1)) {
-        path = ` ${path} L ${pos[0]} ${pos[1]}`;
-    }
+  let path = `M ${points[0][0]} ${points[0][1]}`;
+  for (let pos of points.slice(1)) {
+    path = ` ${path} L ${pos[0]} ${pos[1]}`;
+  }
 
-    return (
-        <path
-            d={path}
-            fill={"lightblue"}
-            pos={pos}
-            onClick={() => console.log("coggers", pos)}
-        />
-    );
+  return (
+    <path
+      d={path}
+      fill={"lightblue"}
+      pos={pos}
+      onClick={() => console.log("coggers", pos)}
+    />
+  );
 }
