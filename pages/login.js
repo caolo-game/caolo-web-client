5aa348c5f31493adfc0adfdebaecbef355567169
Daniel Kiss <littlesnorrboy@gmail.com>
2021-05-03 20:56:52 +0200

    Switch formik to hooks forms

--- pages/login.js
+++ pages/login.js
@@ -1,104 +1,87 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Formik } from "formik";
+import { useForm } from "react-hook-form";
 
 export default function LoginPage({ apiUrl }) {
   const token = useSelector((state) => state?.user?.token);
   const dispatch = useDispatch();
 
   const [err, setErr] = useState(null);
+  const [isSubmitting, setSubmitting] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = useCallback((values) => {
+    setSubmitting(true);
+    (async () => {
+      try {
+        const data = new URLSearchParams();
+        data.append("username", values.username);
+        data.append("password", values.password);
+        const response = await fetch(`${apiUrl}/token`, {
+          method: "POST",
+          mode: "cors",
+          cache: "no-cache",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          referrerPolicy: "no-referrer",
+          body: data,
+        });
+
+        const responseBody = await response.json();
+
+        console.debug(responseBody);
+        switch (response.status) {
+          case 200:
+            dispatch({
+              type: "USER.SET_TOKEN",
+              token: responseBody.access_token,
+            });
+            break;
+          case 400:
+            setErr(responseBody.detail);
+            break;
+          default:
+            console.error("Unhandled status code", response);
+            setErr("Unhandled error while submitting");
+        }
+      } catch (err) {
+        console.warn("Failed to log in", err);
+      }
+
+      setSubmitting(false);
+    })();
+  });
 
   if (!token)
     return (
-      <Formik
-        initialValues={{ username: "", password: "" }}
-        validate={(values) => {
-          const errors = {};
-          if (!values.username) errors.username = "Required";
-          if (!values.password) errors.password = "Required";
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          (async () => {
-            try {
-              const data = new URLSearchParams();
-              data.append("username", values.username);
-              data.append("password", values.password);
-              const response = await fetch(`${apiUrl}/token`, {
-                method: "POST",
-                mode: "cors",
-                cache: "no-cache",
-                headers: {
-                  "Content-Type": "application/x-www-form-urlencoded",
-                },
-                referrerPolicy: "no-referrer",
-                body: data,
-              });
-
-              const responseBody = await response.json();
-
-              console.debug(responseBody);
-              switch (response.status) {
-                case 200:
-                  dispatch({
-                    type: "USER.SET_TOKEN",
-                    token: responseBody.access_token,
-                  });
-                  break;
-                case 400:
-                  setErr(responseBody.detail);
-                  break;
-                default:
-                  console.error("Unhandled status code", response);
-                  setErr("Unhandled error while submitting");
-              }
-            } catch (err) {
-              console.warn("Failed to log in", err);
-            }
-
-            setSubmitting(false);
-          })();
-        }}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-        }) => (
-          <form onSubmit={handleSubmit}>
-            {err ? err : null}
-            <div>
-              <label>Username</label>
-              <input
-                type="text"
-                name="username"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.username}
-              ></input>
-              {errors.username && touched.username && errors.username}
-            </div>
-            <div>
-              <label>Password</label>
-              <input
-                type="password"
-                name="password"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.password}
-              ></input>
-              {errors.password && touched.password && errors.password}
-            </div>
-            <button type="submit" disabled={isSubmitting}>
-              Log in
-            </button>
-          </form>
-        )}
-      </Formik>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {err ? err : null}
+        <div>
+          <label>Username</label>
+          <input
+            type="text"
+            {...register("username", { required: true })}
+          ></input>
+          {errors.username && touched.username && errors.username}
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            type="password"
+            {...register("password", { required: true })}
+          ></input>
+          {errors.password && touched.password && errors.password}
+        </div>
+        <button type="submit" disabled={isSubmitting}>
+          Log in
+        </button>
+      </form>
     );
   else return "Hello user";
 }
--- pages/register.js
+++ pages/register.js
@@ -1,118 +1,92 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Formik } from "formik";
+import { useForm } from "react-hook-form";
 
 export default function RegisterPage({ apiUrl }) {
   const token = useSelector((state) => state?.user?.token);
   const dispatch = useDispatch();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
 
   const [err, setErr] = useState(null);
+  const [isSubmitting, setSubmitting] = useState(false);
+
+  const onSubmit = useCallback(
+    (values) => {
+      setSubmitting(true);
+      (async () => {
+        try {
+          const response = await fetch(`${apiUrl}/register`, {
+            method: "POST",
+            body: JSON.stringify(values),
+          });
+
+          const responseBody = await response.json();
+
+          console.debug(responseBody);
+          switch (response.status) {
+            case 200:
+              dispatch({
+                type: "USER.SET_TOKEN",
+                token: responseBody.access_token,
+              });
+              break;
+            case 400:
+              setErr(responseBody.detail);
+              break;
+            case 422:
+              setErr(
+                responseBody.detail
+                  .map((x) => `${x.loc[1]}: ${x.msg}`)
+                  .join(",")
+              );
+              break;
+            default:
+              console.error("Unhandled status code", response);
+              setErr("Unhandled error while submitting");
+          }
+        } catch (err) {
+          console.warn("Failed to register", err);
+        }
+        setSubmitting(false);
+      })();
+    },
+    [setSubmitting, dispatch]
+  );
+
+  console.log(errors);
 
   return token ? (
     "hello user"
   ) : (
-    <Formik
-      initialValues={{ username: "", pw: "", email: "" }}
-      validate={(values) => {
-        const errors = {};
-        if (!values.username) errors.username = "Required";
-        if (!values.email) errors.email = "Required";
-        if (values.email.indexOf("@") < 0)
-          errors.email = "Please enter a valid email address";
-        if (!values.pw) errors.pw = "Required";
-        return errors;
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        (async () => {
-          try {
-            const response = await fetch(`${apiUrl}/register`, {
-              method: "POST",
-              body: JSON.stringify(values),
-            });
-
-            const responseBody = await response.json();
-
-            console.debug(responseBody);
-            switch (response.status) {
-              case 200:
-                dispatch({
-                  type: "USER.SET_TOKEN",
-                  token: responseBody.access_token,
-                });
-                break;
-              case 400:
-                setErr(responseBody.detail);
-                break;
-              case 422:
-                setErr(
-                  responseBody.detail
-                    .map((x) => `${x.loc[1]}: ${x.msg}`)
-                    .join(",")
-                );
-                break;
-              default:
-                console.error("Unhandled status code", response);
-                setErr("Unhandled error while submitting");
-            }
-          } catch (err) {
-            console.warn("Failed to register", err);
-          }
-          setSubmitting(false);
-        })();
-      }}
-    >
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-      }) => (
-        <form onSubmit={handleSubmit}>
-          {err ? err : null}
-          <div>
-            <label>Email</label>
-            <input
-              type="email"
-              name="email"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.email}
-            ></input>
-            {errors.email && touched.email && errors.email}
-          </div>
-          <div>
-            <label>Username</label>
-            <input
-              type="text"
-              name="username"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.username}
-            ></input>
-            {errors.username && touched.username && errors.username}
-          </div>
-          <div>
-            <label>Password</label>
-            <input
-              type="password"
-              name="pw"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.pw}
-            ></input>
-            {errors.pw && touched.pw && errors.pw}
-          </div>
-          <div>
-            <button type="submit" disabled={isSubmitting}>
-              Sign up
-            </button>
-          </div>
-        </form>
-      )}
-    </Formik>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {err ? err : null}
+      <div>
+        <label>Email</label>
+        <input type="email" {...register("email", { required: true })}></input>
+        {errors.email && errors.email.type}
+      </div>
+      <div>
+        <label>Username</label>
+        <input
+          type="text"
+          {...register("username", { required: true })}
+        ></input>
+        {errors.username && errors.username.type}
+      </div>
+      <div>
+        <label>Password</label>
+        <input type="password" {...register("pw", { required: true })}></input>
+        {errors.pw && errors.pw.type}
+      </div>
+      <div>
+        <input type="submit" />
+      </div>
+    </form>
   );
 }
 
